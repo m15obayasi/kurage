816bde47a89b58cf54c3ba8c5d4781bd944e63ae
m15obayasi <63591817+m15obayasi@users.noreply.github.com>
2024-10-29 23:50:21 +0900

    アップロード
    アップロードしました。わかんねえけど。

---
+++ js/kurage.js
@@ -0,0 +1,126 @@
+$(".button").on("click", function () {
+    $(".boto").fadeOut();
+    var num = Math.floor(Math.random() * 60);
+    if (num === 0) {
+        $(".aisu").html("スウィートポテマロパンプキン？");
+    } else if (num === 1) {
+        $(".aisu").html("マンゴタンゴ？");
+    } else if (num === 2) {
+        $(".aisu").html("ティーオーレ？");
+    } else if (num === 3) {
+        $(".aisu").html("コットンキャンディ？");
+    } else if (num === 4) {
+        $(".aisu").html("レモンシャーベット？");
+    } else if (num === 5) {
+        $(".aisu").html("ファンキーフランケン？");
+    } else if (num === 6) {
+        $(".aisu").html("マロンマロン？");
+    } else if (num === 7) {
+        $(".aisu").html("チョコレートチップ？");
+    } else if (num === 8) {
+        $(".aisu").html("マスカットバスケット？");
+    } else if (num === 9) {
+        $(".aisu").html("ホッピングシャワー？");
+    } else if (num === 10) {
+        $(".aisu").html("ジャモカコーヒー？");
+    } else if (num === 11) {
+        $(".aisu").html("ストロベリーチーズケーキ？");
+    } else if (num === 12) {
+        $(".aisu").html("キャラメルリボン？");
+    } else if (num === 13) {
+        $(".aisu").html("マスクメロン？");
+    } else if (num === 14) {
+        $(".aisu").html("チョップドチョコレート？");
+    } else if (num === 15) {
+        $(".aisu").html("ラムレーズン？");
+    } else if (num === 16) {
+        $(".aisu").html("バナナアンドストロベリー？");
+    } else if (num === 17) {
+        $(".aisu").html("ジャモカアーモンドファッジ？");
+    } else if (num === 18) {
+        $(".aisu").html("チョコレートミント？");
+    } else if (num === 19) {
+        $(".aisu").html("ナッツトゥユー？");
+    } else if (num === 20) {
+        $(".aisu").html("大納言あずき？");
+    } else if (num === 21) {
+        $(".aisu").html("クッキーアンドクリーム？");
+    } else if (num === 22) {
+        $(".aisu").html("オレンジソルベ？");
+    } else if (num === 23) {
+        $(".aisu").html("ベリベリーストロベリー？");
+    } else if (num === 24) {
+        $(".aisu").html("ロッキーロード？");
+    } else if (num === 25) {
+        $(".aisu").html("ラブポーションサーティワン？");
+    } else if (num === 26) {
+        $(".aisu").html("スイカサマー？");
+    } else if (num === 27) {
+        $(".aisu").html("ストロベリースペシャルタイム？");
+    } else if (num === 28) {
+        $(".aisu").html("コーヒーコーヒーチップ？");
+    } else if (num === 29) {
+        $(".aisu").html("レモンマスカルポーネチーズケーキ？");
+    } else if (num === 30) {
+        $(".aisu").html("フルーツフュージョン？");
+    } else if (num === 31) {
+        $(".aisu").html("レモンクランベリーチーズケーキ？");
+    } else if (num === 32) {
+        $(".aisu").html("オーレ＆オーレ？");
+    } else if (num === 33) {
+        $(".aisu").html("キャラメルプラリネチーズケーキ？");
+    } else if (num === 34) {
+        $(".aisu").html("ブルーベリーパンナコッタ？");
+    } else if (num === 35) {
+        $(".aisu").html("トリプルチョコレートパッション");
+    } else if (num === 36) {
+        $(".aisu").html("ツインベリーチーズケーキ？");
+    } else if (num === 37) {
+        $(".aisu").html("テンプテーションアイランド？");
+    } else if (num === 38) {
+        $(".aisu").html("ハットナッツトリック？");
+    } else if (num === 39) {
+        $(".aisu").html("ロックンボールチョコレート？");
+    } else if (num === 40) {
+        $(".aisu").html("ドルチェ・ビータ？");
+    } else if (num === 41) {
+        $(".aisu").html("ニューヨークチーズケーキ？");
+    } else if (num === 42) {
+        $(".aisu").html("パンプキンプリン？");
+    } else if (num === 43) {
+        $(".aisu").html("ワイルドアマゾン？");
+    } else if (num === 44) {
+        $(".aisu").html("マムズアップルパイ ？");
+    } else if (num === 45) {
+        $(".aisu").html("ピーチクーラー？");
+    } else if (num === 46) {
+        $(".aisu").html("ラプソディインブルーベリー？");
+    } else if (num === 47) {
+        $(".aisu").html("モンキーマッシュ？");
+    } else if (num === 48) {
+        $(".aisu").html("パイナップルナッツ？");
+    } else if (num === 49) {
+        $(".aisu").html("ダイキュリーアイス？");
+    } else if (num === 50) {
+        $(".aisu").html("キーライムタイム？");
+    } else if (num === 51) {
+        $(".aisu").html("チョコレートムースロイヤル？");
+    } else if (num === 52) {
+        $(".aisu").html("ハートフルハート？");
+    } else if (num === 53) {
+        $(".aisu").html("ウインターホワイトチョコレート？");
+    } else if (num === 54) {
+        $(".aisu").html("ピースオブケーキ？");
+    } else if (num === 55) {
+        $(".aisu").html("キャラメルプレリンクリーム？");
+    } else if (num === 56) {
+        $(".aisu").html("バーガンディチェリー？");
+    } else if (num === 57) {
+        $(".aisu").html("ハワイアンクランチ？");
+    } else if (num === 58) {
+        $(".aisu").html("ピンクバブルガム？");
+    } else if (num === 59) {
+        $(".aisu").html("ネオンモンスター？");
+    }
+
+});
